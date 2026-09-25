f8220c83aaa19efc911e3baf3ca85a3ebad6bceb
liujiawei-zuopin <liujiawei@users.noreply.github.com>
2026-09-26 05:08:00 +0800

    fix(build): integrate self-contained Media3 extensions and direct CI workflow

--- app/src/main/java/com/fongmi/android/tv/player/exo/ExoMediaSourceFactory.java
+++ app/src/main/java/com/fongmi/android/tv/player/exo/ExoMediaSourceFactory.java
@@ -13,7 +13,6 @@ import androidx.media3.datasource.cache.LeastRecentlyUsedCacheEvictor;
 import androidx.media3.datasource.cache.SimpleCache;
 import androidx.media3.datasource.okhttp.OkHttpDataSource;
 import androidx.media3.exoplayer.drm.DrmSessionManagerProvider;
-import androidx.media3.exoplayer.libass.LibassPlaybackSession;
 import androidx.media3.exoplayer.source.DefaultMediaSourceFactory;
 import androidx.media3.exoplayer.source.MediaSource;
 import androidx.media3.exoplayer.source.preload.MediaSourceFactorySupplier;
@@ -38,17 +37,15 @@ public class ExoMediaSourceFactory implements MediaSource.Factory {
     private static Cache cache;
 
     private final DefaultMediaSourceFactory defaultMediaSourceFactory;
-    private final LibassPlaybackSession libassPlaybackSession;
 
     private HttpDataSource.Factory httpDataSourceFactory;
     private DataSource.Factory dataSourceFactory;
 
-    private ExoMediaSourceFactory(LibassPlaybackSession libassPlaybackSession) {
-        this.libassPlaybackSession = libassPlaybackSession;
+    private ExoMediaSourceFactory() {
         this.defaultMediaSourceFactory = new DefaultMediaSourceFactory(getDataSourceFactory(), createDefaultExtractorsFactory());
     }
 
-    static MediaSourceFactorySupplier supplier(LibassPlaybackSession libassPlaybackSession) {
+    static MediaSourceFactorySupplier supplier() {
         return new MediaSourceFactorySupplier() {
             @NonNull
             @Override
@@ -64,7 +61,7 @@ public class ExoMediaSourceFactory implements MediaSource.Factory {
 
             @Override
             public MediaSource.Factory get() {
-                return new ExoMediaSourceFactory(libassPlaybackSession);
+                return new ExoMediaSourceFactory();
             }
         };
     }
@@ -115,9 +112,7 @@ public class ExoMediaSourceFactory implements MediaSource.Factory {
     @Override
     public MediaSource createMediaSource(@NonNull MediaItem mediaItem) {
         getHttpDataSourceFactory().setDefaultRequestProperties(ExoUtil.extractHeaders(mediaItem));
-        if (!libassPlaybackSession.isAvailable()) return defaultMediaSourceFactory.createMediaSource(mediaItem);
-        LibassPlaybackSession.MediaComponents components = libassPlaybackSession.createMediaComponents(mediaItem, createDefaultExtractorsFactory());
-        return new DefaultMediaSourceFactory(getDataSourceFactory(), components.extractorsFactory).setSubtitleParserFactory(components.subtitleParserFactory).createMediaSource(mediaItem);
+        return defaultMediaSourceFactory.createMediaSource(mediaItem);
     }
 
     private static ExtractorsFactory createDefaultExtractorsFactory() {
