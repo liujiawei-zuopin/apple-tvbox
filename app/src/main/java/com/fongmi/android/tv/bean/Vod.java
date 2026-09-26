4fa2185154789be270b6fe5c5067993541771801
liujiawei-zuopin <liujiawei@users.noreply.github.com>
2026-09-26 18:04:43 +0800

    fix(model): add setRemarks and standard setters to Vod bean

--- app/src/main/java/com/fongmi/android/tv/bean/Vod.java
+++ app/src/main/java/com/fongmi/android/tv/bean/Vod.java
@@ -155,12 +155,24 @@ public class Vod implements Parcelable, Diffable<Vod> {
         return TextUtils.isEmpty(vodRemarks) ? "" : vodRemarks.trim();
     }
 
+    public void setRemarks(String vodRemarks) {
+        this.vodRemarks = vodRemarks;
+    }
+
     public String getYear() {
         return TextUtils.isEmpty(vodYear) ? "" : vodYear.trim();
     }
 
+    public void setYear(String vodYear) {
+        this.vodYear = vodYear;
+    }
+
     public String getArea() {
         return TextUtils.isEmpty(vodArea) ? "" : vodArea.trim();
+    }
+
+    public void setArea(String vodArea) {
+        this.vodArea = vodArea;
     }
 
     public String getDirector() {
